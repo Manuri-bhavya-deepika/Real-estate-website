7119278aa9b2065c03b527f7f3f1664872cc1112
Bhavya Deepika Manuri <84456130+Manuri-bhavya-deepika@users.noreply.github.com>
2023-08-03 11:30:35 +0530

    Update Aboutt.jsx

--- FrontEnd/src/components/about/Aboutt.jsx
+++ FrontEnd/src/components/about/Aboutt.jsx
@@ -13,13 +13,13 @@ const Aboutt = () => {
         <div className='container flex mtop'>
           <div className='left row'>
             <Heading title='Our Agency' subtitle='Check out our company story and work process' /> 
-            <p>Our story began [insert year] when our founder [insert name] recognized a need for a more personalized approach to real estate. 
-            With a background in [insert relevant experience], [insert name] set out to build a team of like-minded professionals who shared a commitment to putting clients first.</p>
+            <p>Our story began  when our founder  recognized a need for a more personalized approach to real estate. 
+            With a background we set out to build a team of like-minded professionals who shared a commitment to putting clients first.</p>
 
-            <p>Today, our team has grown to include [insert number] agents, all of whom are experts in the local real estate market. 
+            <p>Today, our team has grown to include agents, all of whom are experts in the local real estate market. 
             We believe in building long-lasting relationships with our clients, and we work hard to earn their trust and respect.</p>
             
-            <p>What sets us apart from other real estate agencies is our dedication to [insert unique selling proposition]. 
+            <p>What sets us apart from other real estate agencies is our dedication to insert unique selling proposition. 
             Whether you're buying or selling a property, we bring a unique perspective and a wealth of knowledge to every transaction. 
             We understand that every client is unique, and we tailor our approach to meet their individual needs.</p>
             <Link to="/About">
